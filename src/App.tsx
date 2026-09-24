0978d2f19f1a5df9c6fbf4f22636c766d4ab25c1
Alireza Shabani <alireza4o4shabani@gmail.com>
2026-09-24 17:33:02 +0330

    install and configure Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-[#08090a]">
+      <h1 className="text-3xl font-bold">
+        <span className="text-red-600">404</span> Compress
+      </h1>
+    </div>
+  );
+}
+
+export default App;
